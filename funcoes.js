9588f223828a39eb5b42cb4ec899e64021628824
Kevin Capocecera <kevin_pereira1999@hotmail.com>
2024-06-20 22:04:09 -0300

    Exercícios de condicional JS

---
+++ funcoes.js
@@ -0,0 +1,75 @@
+function pulaLinha (){
+    document.write("<br><br>");
+}
+function soma (a, b){
+    return a + b
+}
+function dobro(a){
+    return (a * 2)
+}
+function area(a, b){
+    return (a*b)/2
+}
+function areaCirculo (a){
+    return (Math.PI * (a*a))
+}
+function temp (a){
+    return ((a * (9/5))+32)
+}
+function imc (a, b){
+    return (a/(b*b))
+}
+function idadeDias (a){
+    return (a*365)
+}
+function razao (a, b){
+    return (a/b)
+}
+function desconto(a){
+    return  (a-(a*0.1))
+}
+function converter(a){
+    return (a*1000)
+}
+function areaRet(a, b){
+    return (a*b)
+}
+function quadrado (a){
+    return (a * a)
+}
+function valorParcela (a, b){
+    return ((a+(a*0.1))/b)
+}
+function resto (a,b){
+    return (a % b )
+}
+function media (a, b, c){
+    return ((a + b + c)/3)
+}
+function troco (a, b){
+    return (a-b)*(-1)
+}
+function salario(a,b){
+    return (areaRet(a,b))
+}
+function aumento (a){
+    return (a+(a*0.2))
+}
+function impar(a){
+    return (a/2)
+}
+function perimetroRet (a, b){
+    return ((a*2) + (b*2))
+}
+function circunferencia(a){
+    return (2 * Math.PI * a)
+}
+function perimetroTri(a, b, c){
+    return (a + b + c)
+}
+function tempC (a){
+    return ((5 / 9) * (a-32))
+}
+function mediaSem(a, b, c, d){
+    return  ((((a+b)/2)+((c+d)/2))/2)
+}
